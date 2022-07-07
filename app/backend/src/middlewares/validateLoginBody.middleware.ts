a4b7d6ad1360b3fdc7de4763b20acdc88ee56521
Breno Santos <brenosantos145@gmail.com>
2022-07-07 17:04:03 -0300

    :sparkles: Create user login schema

---
+++ app/backend/src/middlewares/validateLoginBody.middleware.ts
@@ -0,0 +1,7 @@
+// import { Request, Response, NextFunction } from 'express';
+
+// const validateLoginBody = (req: Request, res: Response, next: NextFunction) => {
+
+// };
+
+// export default validateLoginBody;
