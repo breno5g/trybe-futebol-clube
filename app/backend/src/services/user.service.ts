a35704571bf7814d38443023251ee927406695f5
Breno Santos <brenosantos145@gmail.com>
2022-07-07 11:51:16 -0300

    :sparkles: Create Bcrypt class

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,8 +1,10 @@
+import JWT from '../utils/jwt';
 import { ILogin } from '../interfaces/index';
 import Model from '../database/models/user';
 
 export default class Service {
   private model = Model;
+  private jwt = new JWT();
 
   login = async (loginData: ILogin) => {
     const user = await this.model.findOne({ where: { email: loginData.email } });
