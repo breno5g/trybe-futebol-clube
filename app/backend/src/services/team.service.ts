da28caaa1ded06e4c07e877f49834a9c26ffb73a
Breno Santos <brenosantos145@gmail.com>
2022-07-07 18:30:27 -0300

    :sparkles: Create getById function in team service

--- app/backend/src/services/team.service.ts
+++ app/backend/src/services/team.service.ts
@@ -5,7 +5,12 @@ export default class Service {
   private model = Model;
 
   getAll = async () => {
-    const teams = await Model.findAll();
+    const teams = await this.model.findAll();
     return teams;
+  };
+
+  getById = async (id: string) => {
+    const team = await this.model.findOne({ where: { id } });
+    return team;
   };
 }
