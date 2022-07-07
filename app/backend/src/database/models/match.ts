6c5e953148074ed4a101e6f59ed9d24a8e456f1e
Breno Santos <brenosantos145@gmail.com>
2022-07-07 18:57:54 -0300

    :sparkles: Create matches model and migrations

---
+++ app/backend/src/database/models/match.ts
@@ -0,0 +1,27 @@
+import { Model, INTEGER, BOOLEAN } from 'sequelize';
+import db from '.';
+import Team from './team';
+
+class Match extends Model {
+  // static associate(models) {
+  // }
+}
+
+Match.init({
+  homeTeam: INTEGER,
+  homeTeamGoals: INTEGER,
+  awayTeam: INTEGER,
+  awayTeamGoals: INTEGER,
+  inProgress: BOOLEAN,
+}, {
+  sequelize: db,
+  modelName: 'match',
+  underscored: true,
+  timestamps: false,
+});
+
+Team.hasMany(Match, { foreignKey: 'homeTeam', as: 'homeMatches' });
+Team.hasMany(Match, { foreignKey: 'awayTeam', as: 'awayMatches' });
+
+Match.belongsTo(Team, { foreignKey: 'homeTeam', as: 'homeTeam' });
+Match.belongsTo(Team, { foreignKey: 'awayTeam', as: 'awayTeam' });
