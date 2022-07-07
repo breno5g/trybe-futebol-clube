d23e7aba1bea2b7368162861eef75e4e920c9ccc
Breno Santos <brenosantos145@gmail.com>
2022-07-07 17:12:23 -0300

    :heavy_plus_sign: Install joi and types

--- app/backend/src/Routes/user.routes.ts
+++ app/backend/src/Routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import validateLoginBody from '../middlewares/validateLoginBody.middleware';
 import Controller from '../controllers/user.controller';
 
 const routes = Router();
 
 const controller = new Controller();
 
-routes.post('/login', controller.login);
+routes.post('/login', validateLoginBody, controller.login);
 
 export default routes;
