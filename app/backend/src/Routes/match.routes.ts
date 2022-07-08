9ac011bec2a7c693ac2fc0faaae2d948b5fda204
Breno Santos <brenosantos145@gmail.com>
2022-07-08 10:19:06 -0300

    :sparkles: Create middleware to validate match body

--- app/backend/src/Routes/match.routes.ts
+++ app/backend/src/Routes/match.routes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import Controller from '../controllers/match.controller';
+import validateMatchBody from '../middlewares/validataMatchBody.middleware';
 
 const routes = Router();
 
 const controller = new Controller();
 
 routes.get('/', controller.getAll);
-routes.post('/', controller.create);
+routes.post('/', validateMatchBody, controller.create);
 
 export default routes;
