2791ab78422844ceabb03a2315b557ef4ef2f7a5
Breno Santos <brenosantos145@gmail.com>
2022-07-08 08:34:47 -0300

    :sparkles: Create get all function in match controller

---
+++ app/backend/src/controllers/match.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import Service from '../services/match.service';
+
+export default class Controller {
+  private service = new Service();
+
+  getAll = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const teams = await this.service.getAll();
+
+      return res.status(200).json(teams);
+    } catch (error) {
+      next(error);
+    }
+  };
+}
